66fd90b6c3d5d1bae8abf8ca894abe1232b4a8a4
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2026-08-29 14:51:17 +0200

    added graceful shutdown

--- cmd/fooder/main.go
+++ cmd/fooder/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
-	"github.com/MarcinBondaruk/fooder/internal/framework/di"
-	"github.com/MarcinBondaruk/fooder/internal/framework/env"
-	"github.com/MarcinBondaruk/fooder/internal/framework/router"
-	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"log/slog"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	"github.com/MarcinBondaruk/fooder/internal/framework/di"
+	"github.com/MarcinBondaruk/fooder/internal/framework/env"
+	"github.com/MarcinBondaruk/fooder/internal/framework/router"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
+	shutdownChan := make(chan os.Signal, 1)
+	signal.Notify(shutdownChan, syscall.SIGKILL, syscall.SIGTERM)
+
 	logHandlerOpts := &slog.HandlerOptions{
 		Level: slog.LevelDebug,
 	}
@@ -47,9 +55,24 @@ func main() {
 		MaxHeaderBytes:    1 << 20, // 1 MB
 	}
 
-	log.Println("Starting http server on port 8080")
-	err = s.ListenAndServe()
+	go func() {
+		log.Println("Starting http server on port 8080")
+		err = s.ListenAndServe()
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal(err)
+		}
+
+		log.Println("server is shutting down")
+	}()
+
+	<-shutdownChan
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	err = s.Shutdown(ctx)
 	if err != nil {
-		log.Fatal()
+		log.Fatalf("error during shutdown: %v", err)
 	}
+
+	log.Println("server shutdown gracefully")
 }
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/MarcinBondaruk/fooder
 
-go 1.23.4
+go 1.27
 
 require github.com/mattn/go-sqlite3 v1.14.24
 
