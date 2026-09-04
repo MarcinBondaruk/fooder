bf6a74e8fbe5a9aa02866cc526bd7c1a516bbcfa
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2026-09-04 14:53:02 +0200

    implement dtos for recipe request and response

--- internal/api/recipe_handler.go
+++ internal/api/recipe_handler.go
@@ -5,6 +5,19 @@ import (
 	"net/http"
 )
 
+type CreateRecipeRequest struct {
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Ingredients []string `json:"ingredients"`
+}
+
+type RecipeResponse struct {
+	ID          int      `json:"id"`
+	Name        string   `json:"name"`
+	Description string   `json:"description"`
+	Ingredients []string `json:"ingredients"`
+}
+
 func CreateRecipe(logger *slog.Logger) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	})
