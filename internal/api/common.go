de05d0450d59e7dc4d7e77df6ad4b69651f4827a
Marcin Bondaruk <marcin.bondaruk@gmail.com>
2026-09-05 21:13:25 +0200

    working on recipe handler refactor

---
+++ internal/api/common.go
@@ -0,0 +1,9 @@
+package api
+
+type ProblemJson struct {
+	Type     string `json:"type"`
+	Title    string `json:"title"`
+	Status   int    `json:"status"`
+	Detail   string `json:"detail,omitempty"`
+	Instance string `json:"instance,omitempty"`
+}
--- internal/api/recipe_handler.go
+++ internal/api/recipe_handler.go
@@ -1,24 +1,63 @@
 package api
 
 import (
+	"encoding/json"
 	"log/slog"
 	"net/http"
+
+	"github.com/MarcinBondaruk/fooder/internal/recipe"
 )
 
-type CreateRecipeRequest struct {
+type RecipeCreate struct {
 	Name        string   `json:"name"`
 	Description string   `json:"description"`
 	Ingredients []string `json:"ingredients"`
 }
 
-type RecipeResponse struct {
+type Recipe struct {
 	ID          int      `json:"id"`
 	Name        string   `json:"name"`
 	Description string   `json:"description"`
 	Ingredients []string `json:"ingredients"`
 }
 
-func CreateRecipe(logger *slog.Logger) http.Handler {
+func CreateRecipe(logger *slog.Logger, recipeSvc *recipe.Service) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var payload RecipeCreate
+		err := json.NewDecoder(r.Body).Decode(&payload)
+		if err != nil {
+			errResp := ProblemJson{
+				Type:     "",
+				Title:    "",
+				Status:   http.StatusBadRequest,
+				Detail:   "",
+				Instance: "",
+			}
+
+			w.Header().Add("Content-Type", "application/problem+json")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(errResp)
+			return
+		}
+
+		recipe, err := recipeSvc.CreateRecipe(r.Context(), recipe.NewRecipe(payload.Name, payload.Description, payload.Ingredients))
+		if err != nil {
+			errResp := ProblemJson{
+				Type:     "",
+				Title:    "",
+				Status:   http.StatusBadRequest,
+				Detail:   "",
+				Instance: "",
+			}
+
+			w.Header().Add("Content-Type", "application/problem+json")
+			w.WriteHeader(http.StatusBadRequest)
+			json.NewEncoder(w).Encode(errResp)
+			return
+
+		}
+
+		w.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(recipe)
 	})
 }
